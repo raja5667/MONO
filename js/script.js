1716d555a1bc84c41619a4b2a2f1fd58dad80aa0
raja5667 <raja2863a@gmail.com>
2026-07-08 06:04:52 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -21,73 +21,78 @@ if (logo) {
     });
 }
 
+// The sparkle/particle effect only exists on the homepage, which has
+// <canvas id="sparkle-canvas">. Skip all of this on pages that don't.
 const canvas = document.getElementById('sparkle-canvas');
-const ctx = canvas.getContext('2d');
-let particles = [];
 
-function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
+if (canvas) {
+    const ctx = canvas.getContext('2d');
+    let particles = [];
 
-window.addEventListener('resize', resizeCanvas);
-resizeCanvas();
-
-class Particle {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.size = Math.random() * 3 + 0.5;
-        this.speedX = Math.random() * 2 - 1;
-        this.speedY = Math.random() * 2 - 1;
-
-        const colors = ['#89a8f5', '#e67d7d'];
-        this.color = colors[Math.floor(Math.random() * colors.length)];
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
     }
 
-    update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-        this.size *= 0.95;
-    }
-    draw() {
-        ctx.shadowBlur = 10;
-        ctx.shadowColor = this.color;
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fill();
+    window.addEventListener('resize', resizeCanvas);
+    resizeCanvas();
 
-        ctx.shadowBlur = 0;
-    }
-}
+    class Particle {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+            this.size = Math.random() * 3 + 0.5;
+            this.speedX = Math.random() * 2 - 1;
+            this.speedY = Math.random() * 2 - 1;
 
-function createParticles(x, y) {
-    if (window.innerWidth <= 768) {
-        for (let i = 0; i < 3; i++) {
-            particles.push(new Particle(x, y));
+            const colors = ['#89a8f5', '#e67d7d'];
+            this.color = colors[Math.floor(Math.random() * colors.length)];
+        }
+
+        update() {
+            this.x += this.speedX;
+            this.y += this.speedY;
+            this.size *= 0.95;
+        }
+        draw() {
+            ctx.shadowBlur = 10;
+            ctx.shadowColor = this.color;
+            ctx.fillStyle = this.color;
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+            ctx.fill();
+
+            ctx.shadowBlur = 0;
         }
     }
-}
 
-document.addEventListener('mousemove', (e) => {
-    createParticles(e.clientX, e.clientY);
-});
-
-document.addEventListener('touchmove', (e) => {
-    if (e.touches.length > 0) {
-        createParticles(e.touches[0].clientX, e.touches[0].clientY);
+    function createParticles(x, y) {
+        if (window.innerWidth <= 768) {
+            for (let i = 0; i < 3; i++) {
+                particles.push(new Particle(x, y));
+            }
+        }
     }
-}, { passive: false });
 
-function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    particles = particles.filter(p => p.size > 0.2);
-    particles.forEach(p => {
-        p.update();
-        p.draw();
+    document.addEventListener('mousemove', (e) => {
+        createParticles(e.clientX, e.clientY);
     });
-    requestAnimationFrame(animate);
-}
 
-animate();
+    document.addEventListener('touchmove', (e) => {
+        if (e.touches.length > 0) {
+            createParticles(e.touches[0].clientX, e.touches[0].clientY);
+        }
+    }, { passive: false });
+
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        particles = particles.filter(p => p.size > 0.2);
+        particles.forEach(p => {
+            p.update();
+            p.draw();
+        });
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+}
